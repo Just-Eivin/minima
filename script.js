cbe7afa7247030201d97cc392ad5d9d3a9167f48
Mike <justeivin227@gmail.com>
2022-08-25 21:37:17 +0200

    Fixes the bug if user opens up a dev tools If user opens up a developer tools, the relative position of canvas to page changes, but the script does not accommodate to that. This is fixed by recalculating this relative position every time user draws.

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ const pixelCanvas = document.getElementById('pixel-canvas');
 const PIXEL_COUNT = 32; // Number of pixels on each side => The actual resolution of the image
 const pixelRatio = pixelCanvas.clientWidth / PIXEL_COUNT;
 const ctx = pixelCanvas.getContext('2d');
-const canvasBoundingClientRect = pixelCanvas.getBoundingClientRect();
+//const canvasBoundingClientRect = pixelCanvas.getBoundingClientRect();
 let isDrawing = false;
 
 function drawPixel(e) {
+    const canvasBoundingClientRect = pixelCanvas.getBoundingClientRect();
     const mousePosition = [e.clientX - canvasBoundingClientRect.left, e.clientY - canvasBoundingClientRect.top]; // mousePosition[0] == canvasX, mousePosition[1] == canvasY
     const pixelCoordX = Math.floor(mousePosition[0] / pixelRatio);
     const pixelCoordY = Math.floor(mousePosition[1] / pixelRatio);
